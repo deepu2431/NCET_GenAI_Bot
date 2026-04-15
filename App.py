f5273288ec581488c541155b7e969c9ca2faa575
deepu2431 <deepakkumar27559@gmail.com>
2026-04-15 11:58:36 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 from groq import Groq
 
 st.set_page_config("Deepak Content Generator",layout="wide")
+st.title("Deepak– Content Generator")
 st.image(""C:\Users\DEEPU\OneDrive\Pictures\images (4).jpeg"")
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
